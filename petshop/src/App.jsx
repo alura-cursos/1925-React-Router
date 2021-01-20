e69585bc476f933ef96d11a2242428d4996bcda1
felipedotcom <felipe.rodolfo29@gmail.com>
2021-01-20 10:02:53 -0300

    projeto_inicial

---
+++ petshop/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/css/base/base.css'
+
+function App() {
+  return (
+  <>
+
+  </>
+  )
+}
+
+export default App
---
+++ petshop/src/paginas/Sobre.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import '../assets/css/componentes/cartao.css'
+
+const Sobre = () => {
+
+  return (
+    <main>
+      <div className="container">
+        <h2 className="titulo-pagina">Sobre Doguito</h2>
+      </div>
+      <section className="container flex flex--centro">
+        <article className="cartao">
+          O blog do doguito foi criado para tirar diversas dúvidas sobre o mundo pet!!!
+        </article>
+        
+      </section>
+    </main>
+  )
+}
+
+export default Sobre
