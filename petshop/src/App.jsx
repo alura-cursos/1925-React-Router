cb47878b29610e220c33b886ebb1277e2e14e9d3
felipedotcom <felipe.rodolfo29@gmail.com>
2021-01-20 10:11:19 -0300

    aula1

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
 
 function App() {
-  return (
-  <>
 
-  </>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/sobre'>
+          <Sobre />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
